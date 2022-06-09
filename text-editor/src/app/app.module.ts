5c76023459f5e8e1a099f0057c0c20a3957043ef
AlionaMu <alionazimaby@gmail.com>
2022-06-09 14:40:01 +0300

    feat: add components

--- text-editor/src/app/app.module.ts
+++ text-editor/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { TagsListComponent } from './components/tags-list/tags-list.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { NoteFormComponent } from './components/note-form/note-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TagsListComponent,
+    NotesListComponent,
+    NoteFormComponent
   ],
   imports: [
     BrowserModule,
